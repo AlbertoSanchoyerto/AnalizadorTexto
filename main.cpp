0081468541c2b2f8068ffd80bac24df5e7e60351
Alberto Sanchoyerto Martinez <42414577+AlbertoSanchoyerto@users.noreply.github.com>
2026-05-09 15:41:15 +0200

    Add files via upload
    Correcciones

--- main.cpp
+++ main.cpp
@@ -6,46 +6,139 @@
 #include "utils/LineaComandos.h"
 
 #include <iostream>
+#include <string>
+#include <direct.h>
+
+#if defined(_WIN32)
+
+//#include <windows.h>
+//#include <direct.h>
+
+#elif defined(__linux__)
+
+#include <unistd.h>
+#include <limits.h>
+
+#elif defined(__APPLE__)
+
+#include <mach-o/dyld.h>
+#include <unistd.h>
+#include <limits.h>
+
+#endif
+
+/**
+ * @brief Obtenemos el directorio del ejecutable (multiplataforma)
+ */
+std::string directorioEjecutable()
+{
+#if defined(_WIN32)
+
+	char buffer[260];
+
+	_getcwd(buffer, 260);
+	std::string path(buffer);
+
+	size_t pos = path.find_last_of("\\/");
+
+#elif defined(__linux__)
+
+	char buffer[1024];
+
+	ssize_t len = readlink("/proc/self/exe", buffer, sizeof(buffer) - 1);
+
+	if (len <= 0)
+		return "";
+	
+	getcwd(buffer, sizeof(buffer));
+	std::string path(buffer);
+
+	size_t pos = path.find_last_of('/');
+
+#elif defined(__APPLE__)
+
+	char buffer[1024];
+
+	getcwd(buffer, sizeof(buffer));
+	std::string path(buffer);
+
+	size_t pos = path.find_last_of('/');
+
+#endif
+
+	if (pos == std::string::npos)
+		return "";
+
+	return path.substr(0, pos);
+}
+
+/**
+ * @brief Nos situamos en el directorio ejecutable (multiplataforma)
+ * asi nos posibilita utilizar directorios relativos al ejecutable
+ */
+void situarseDirectorioEjecutable()
+{
+	std::string directorio = directorioEjecutable();
+
+#if defined(_WIN32)
+
+	_chdir(directorio.c_str());
+
+#else
+
+	chdir(directorio.c_str());
+
+#endif
+}
 
 int main(int argc, char* argv[]) {
 
-    LineaComandos lineaComandos(argc, argv);
+	/**
+	 @note. Nos situamos en el directorio de la aplicación para que todas las rutas sean relativas al mismo.
+	 */
+	//situarseDirectorioEjecutable();
 
-    lineaComandos.ponerComando("--help", [](const std::vector < std::string>&) {
-        std::cout << "Comandos:\n";
-        std::cout << "  --archivo <ruta>\n";
-        std::cout << "  --log <archivo>\n";
-        std::cout << "  --help\n";
-    });
+	LineaComandos lineaComandos(argc, argv);
 
-    lineaComandos.ponerComando("--log", [](const std::vector < std::string>& args) {
-        if (args.size() < 1) {
-            std::cerr << "Falta archivo de log\n";
-            return;
-        }
+	lineaComandos.ponerComando("--help", [](const std::vector < std::string>&) {
+		std::cout << "Comandos:\n";
+		std::cout << "  --archivo <ruta>\n";
+		std::cout << "  --log <archivo>\n";
+		std::cout << "  --help\n";
+	});
 
-        Logger::instancia().setArchivo(args[0]);
-        std::cout << "Log configurado en " << args[0] << "\n";
-    });
+	lineaComandos.ponerComando("--log", [](const std::vector < std::string>& args) {
+		if (args.size() < 1) {
+			std::cerr << "Falta archivo de log\n";
+			return;
+		}
 
-    lineaComandos.ponerComando("--archivo", [](const std::vector < std::string>& args) {
-        std::string archivoLog =
-        if (args.size() < 1) {
-            std::cerr << "Falta ruta del archivo\n";
-            return;
-        }
+		Logger::instancia().setArchivo(args[0]);
+		std::cout << "Log configurado en " << args[0] << "\n";
+	});
 
-        Logger::instancia().setArchivo("AnalizadorTexto.log");
-        Logger::instancia().setNivel(NivelLog::DEBUG);
-        
-        LOG_INFO("Inicio del programa");
+	lineaComandos.ponerComando("--archivo", [](const std::vector < std::string>& args) {
+		if (args.size() < 1) {
+			std::cerr << "Falta ruta del archivo\n";
+			return;
+		}
 
-        DocumentoTexto doc;
-        doc.cargarDesdeFichero(args[0]);
-        doc.imprimir();
-    });
+		Logger::instancia().setArchivo("AnalizadorTexto.log");
+		Logger::instancia().setNivel(NivelLog::DEBUG);
 
-    LineaComando.ejecutar();
+		LOG_INFO("Inicio del programa");
 
-    return 0;
+		try {
+			DocumentoTexto doc;
+			doc.cargarDesdeArchivo(args[0]);
+			doc.imprimir();
+		}
+		catch (const Error& e) {
+			LOG_ERROR(e.what());
+		}
+	});
+
+	lineaComandos.ejecutar();
+
+	return 0;
 };
