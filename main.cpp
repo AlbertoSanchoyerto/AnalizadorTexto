346d1c97616deb89631a3f13d4d28ebe3a8b5698
Alberto Sanchoyerto Martinez <42414577+AlbertoSanchoyerto@users.noreply.github.com>
2026-05-09 12:24:50 +0200

    Actualizar main.cpp

--- main.cpp
+++ main.cpp
@@ -3,22 +3,49 @@
 */
 #include "core/DocumentoTexto.h"
 #include "utils/ErrorMacros.h"
+#include "utils/lineaComandos.h"
 
 #include <iostream>
 
-int main() {
+int main(int argc, char* argv[]) {
 
-    Logger::instancia().setArchivo("AnalizadorTexto.log");
-    Logger::instancia().setNivel(NivelLog::DEBUG);
+    LineaComando lineaComando(argc, argv);
 
-    LOG_INFO("Inicio del programa");
+    lineaComando.ponerComando("--help", [](const std::vector < std::string>&) {
+        std::cout << "Comandos:\n";
+        std::cout << "  --archivo <ruta>\n";
+        std::cout << "  --log <archivo>\n";
+        std::cout << "  --help\n";
+    });
 
-    try {
+    lineaComando.ponerComando("--log", [](const std::vector < std::string>& args) {
+        if (args.size() < 1) {
+            std::cerr << "Falta archivo de log\n";
+            return;
+        }
+
+        Logger::instancia().setArchivo(args[0]);
+        std::cout << "Log configurado en " << args[0] << "\n";
+    });
+
+    lineaComando.ponerComando("--archivo", [](const std::vector < std::string>& args) {
+        std::string archivoLog =
+        if (args.size() < 1) {
+            std::cerr << "Falta ruta del archivo\n";
+            return;
+        }
+
+        Logger::instancia().setArchivo("AnalizadorTexto.log");
+        Logger::instancia().setNivel(NivelLog::DEBUG);
+        
+        LOG_INFO("Inicio del programa");
+
         DocumentoTexto doc;
-        doc.cargarDesdeArchivo("texto.txt");
+        doc.cargarDesdeFichero(args[0]);
         doc.imprimir();
-    }
-    catch (const Error& e) {
-        LOG_ERROR(e.what());
-    }
-}
+    });
+
+    LineaComando.ejecutar();
+
+    return 0;
+};
